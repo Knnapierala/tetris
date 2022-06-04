7a29308358f3f524b2e9e5166826e6c230d4afed
Knnapierala <knnapierala@wp.pl>
2022-06-04 13:49:50 +0200

    added arrays of tetrominos

--- script.js
+++ script.js
@@ -3,3 +3,53 @@ let squares = Array.from(document.querySelectorAll('.grid div'));
 const ScoreDisplay = document.querySelector('#score');
 const StartBtn = document.querySelector('#start-button');
 const width = 10;
+
+
+// The Elements
+const lTetromino = [
+  [1, width+1, width*2+1, 2],
+  [width, width+1, width+2. width*2+2],
+  [1, width+1, width*2, width*2+1],
+  [width, width*2, width*2+1, width*2+2]
+]
+
+const zTertomino =[
+  [width*2, width+1, width*2+1,width+2],
+  [0, width, width+1, width*2+1],
+  [width*2, width+1, width*2+1,width+2],
+  [0, width, width+1, width*2+1]
+]
+
+const oTetromino = [
+  [0,1,width,width+1],
+  [0,1,width,width+1],
+  [0,1,width,width+1],
+  [0,1,width,width+1]
+]
+
+const iTetromino =[
+  [1,width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3],
+  [1,width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3]
+]
+
+const tTetromino =[
+  [1, width,width+1,width+2],
+  [1, width+1,width+2,width*2+1],
+  [width,width+1,width+2,width*2+1],
+  [1,width,width+1,width*2+1]
+]
+
+const theTetromino = [lTetromino, zTertomino, oTetromino, iTetromino, tTetromino]
+
+let currentPosition = 4;
+let current = theTetromino[0][0];
+
+//draw the first items
+
+function draw () {
+  current.forEach( index => {squares[currentPosition + index].classList.add('tetromino')})
+}
+
+draw();
