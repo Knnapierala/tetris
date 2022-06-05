75fa6429e77ba45588e23865962b78baa2a3f24b
Knnapierala <knnapierala@wp.pl>
2022-06-05 23:21:44 +0200

    added full row removal and update score variable plus adding new row to the grid

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const startBtn = document.querySelector('#start-button');
 const width = 10;
 let nextRandom = 0
 let timerId
+let score = 0
 
 // The Elements
 const lTetromino = [
@@ -97,6 +98,7 @@ function freeze() {
     currentPosition = 4;
     draw()
     displayShape()
+    addScore()
   }
 }
 
@@ -170,3 +172,25 @@ startBtn.addEventListener('click', () => {
     displayShape()
   }
 })
+
+// score
+function addScore() {
+  for (let i = 0; i < 199; i +=width) {
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+    if(row.every(index => squares[index].classList.contains('taken'))) {
+      score += 10
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+      squares[index].classList.remove('taken')
+      squares[index].classList.remove('tetromino')
+    })
+
+      const squaresRemoved = squares.splice(i, width)
+      squares = squaresRemoved.concat(squares)
+      squares.forEach(cell => grid.appendChild(cell))
+    }
+    
+  }
+}
+
